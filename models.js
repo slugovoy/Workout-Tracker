6b27c6cb5f7c74cd0ea86605b40228f30ce7d428
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2021-01-04 19:07:06 -0700

    added schema

---
+++ models.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+      },
+      name: {
+        type: String,
+        trim: true,
+      },
+      duration: Number,
+      weight: 100,
+      reps: {
+        type: Number,
+        default: 0,
+      },
+      sets: {
+        type: Number,
+        default: 0,
+      },
+      distance: {
+        type: Number,
+        default: 0,
+      },
+      totalDuration: {
+        type: Number,
+        default: 0,
+      },
+    },
+  ],
+});
+
+const workout = mongoose.model("WorkoutSchema", WorkoutSchema);
+
+module.exports = workout;
