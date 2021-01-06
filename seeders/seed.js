87fc2985118121cd146112f92d5ae211d2fee992
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2021-01-06 13:38:24 -0700

    added path

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,9 +1,11 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 let workoutSeed = [
